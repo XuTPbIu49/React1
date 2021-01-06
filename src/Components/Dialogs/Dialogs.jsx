aaf81a310083bfb621aed8e37e79699ca5f12b1b
XuTPbIu49 <XuTPbIu49@gmail.com>
2021-01-07 01:21:13 +0300

    Массив

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -8,42 +8,34 @@ const DialogItem = (props) => {
         <NavLink to={line} activeClassName={s.active}>{props.name}</NavLink>
     </div>
 }
-const Message = (props) => {
-    return <div className={s.massage}>{props.text}</div>
-}
 const Dialogs = (props) => {
-    let DialogsData = [
-        { id: 1, name: "Олег"},
+    const Message = (props) => {
+        return <div className={s.massage}>{props.message}</div>
+    }
+    let dialogs = [
+        {id: 1, name: "Олег"},
         {id: 2, name: "Саня"},
         {id: 3, name: "Валера"},
         {id: 4, name: "Олень"},
         {id: 5, name: "Тракторист"},
         {id: 6, name: "Ubivator3000"}
-    ]
-    let MessegeData = [
-        { id: 1, message: "Олег"},
-        {id: 2, message: "Саня"},
-        {id: 3, message: "Валера"},
-        {id: 4, message: "Олень"},
-        {id: 5, message: "Тракторист"},
-        {id: 6, message: "Ubivator3000"}
-    ]
+    ];
+    let messages = [
+        {id: 1, message: "Привет"},
+        {id: 2, message: "Где мои деньги"},
+        {id: 3, message: "Гавнюююк!!!!"},
+        {id: 4, message: "Аванс пришел!!!!"},
+        {id: 5, message: "Аванс пришел!!!!"}
+    ];
+    let dialogsElements = dialogs.map( d => <DialogItem name={d.name} id={d.id}/>);
+    let messagesElements = messages.map(m => <Message message={m.message}/>);
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItem}>
-                <DialogItem name={DialogsData[0].name} id={DialogsData[0].id}/>
-                <DialogItem name={DialogsData[1].name} id={DialogsData[1].id}/>
-                <DialogItem name={DialogsData[2].name} id={DialogsData[2].id}/>
-                <DialogItem name={DialogsData[3].name} id={DialogsData[3].id}/>
-                <DialogItem name={DialogsData[4].name} id={DialogsData[4].id}/>
-                <DialogItem name={DialogsData[5].name} id={DialogsData[5].id}/>
-
+                {dialogsElements}
             </div>
             <div className={s.massages}>
-                <Message text="Привет"/>
-                <Message text="Где мои деньги?"/>
-                <Message text="Гавнюююк!!!!"/>
-                <Message text="Аванс пришел!!!!"/>
+                {messagesElements}
             </div>
         </div>
     )
--- src/Components/Profile/Mypost/Mypost.jsx
+++ src/Components/Profile/Mypost/Mypost.jsx
@@ -3,7 +3,13 @@ import s from './Mypost.module.css';
 import Post from './Post/Post';
 
 
-let Mypost = () => {
+const Mypost = (props) => {
+    let posts = [
+        {id: 1, message: "Hi, Pidor", LikesCount: '😍2'},
+        {id: 2, message: "LoL", LikesCount: '😍5'},
+        {id: 3, message: "her", LikesCount: '😍0'}
+    ]
+    let postsElements = posts.map(p => <Post massage={p.message} LikesCount={p.LikesCount}/>);
     return (
         <div className={s.item}>
             <div className={s.addpost}>
@@ -18,9 +24,7 @@ let Mypost = () => {
                 </div>
             </div>
             <div className={s.post}>
-                <Post massage='Hi, Pidor' LikesCount='😍2'/>
-                <Post massage='LoL' LikesCount='😍5'/>
-                <Post massage='her' LikesCount='😍0'/>
+                {postsElements}
             </div>
         </div>
     )
